11be44b80d35ef8e76f3fa30102369ec61bf4ca8
alvarorestrepo <alvarorestrepoz@gmail.com>
2020-08-19 20:17:38 -0500

    fin del proyecto de citas veterinarias en localStorage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
 
 function App() {
+  //Citas en el localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+  
+  // Arreglo de citas 
+  const [citas, guardarCitas] = useState(citasIniciales);
+  
+  // Use Effect para realizar ciertas operaciones cuando el state cambia 
+  useEffect( () => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+   if (citasIniciales) {
+     localStorage.setItem('citas', JSON.stringify(citas))
+   }else{
+     localStorage.setItem('citas', JSON.stringify([]));
+   }
+  },[citas]);
+
+  // Funcion que toma las citas actuales y agregue la nueva 
+  const crearCita = cita =>{
+    guardarCitas([
+      ...citas,
+      cita
+    ])
+  }
+
+  // Funcion que elimina una cita por su ID 
+  const eliminarCita = id =>{
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  // Mensaje condicional para agregar el titulo de las citas 
+  const titulo = citas.length === 0 ?'No hay citas':'Administra tus citas'
+
+
   return (
     <Fragment>
       <h1>administrador de pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-          <Formulario/>
+          <Formulario
+          crearCita={crearCita}
+          />
           </div>
           <div className="one-half column">
-          2
+          <h2>{titulo}</h2>
+          {citas.map(cita =>(
+            <Cita
+            key={cita.id}
+            cita={cita}
+            eliminarCita={eliminarCita}
+            />
+          ))}
           </div>
         </div>
       </div>
